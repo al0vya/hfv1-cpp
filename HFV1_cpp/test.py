d47ddd64af0bf3a4fc8748e2bae80fe78dbbc1ee
Alovya Chowdhury <alovya.chowdhury@gmail.com>
2021-12-20 19:41:27 +0000

    add automated functional testing and running script

--- HFV1_cpp/test.py
+++ HFV1_cpp/test.py
@@ -11,11 +11,30 @@ from mpl_toolkits.mplot3d import Axes3D
 def EXIT_HELP():
     help_message = (
         "Use as:\n\n" +
-        "python test.py plot <MODE>\n" +
-        "MODE : [debug,release]"
+        "    python test.py test <MODE> <NUM_CELLS> <REFINEMENT_LEVEL> <EPSILON>\n\n" +
+        "        MODE : [debug,release]\n\n"
+        "    python test.py run <MODE> <TEST_CASE> <NUM_CELLS> <REFINEMENT_LEVEL> <EPSILON>\n\n" +
+        "        MODE      : [debug,release]\n" +
+        "        TEST_CASE : [1,2,3,4,5,6]\n\n" +
+        "    Available test cases:\n" +
+        "        1. Wet dam break\n" +
+        "        2. Dry dam break\n" +
+        "        3. Dry dam break with friction\n" +
+        "        4. Wet c property\n" +
+        "        5. Wet dry c property\n" +
+        "        6. Building overtopping"
     )
     
     sys.exit(help_message)
+
+test_names = [
+    "wet-dam-break",
+    "dry-dam-break",
+    "dry-dam-break-fric",
+    "wet-c-prop",
+    "wet-dry-c-prop",
+    "building-overtopping",
+]
 
 def plot_soln(
         x1,
@@ -24,8 +43,8 @@ def plot_soln(
         quantity,
         length,
         ylabel,
-        test_num=0,
-        test_name="ad-hoc",
+        test_num,
+        test_name
     ):
         filename = str(test_num) + "-" + test_name + "-" + quantity
         
@@ -46,18 +65,19 @@ def plot_soln(
 class Solution:
     def __init__(
         self,
-        mode
+        mode,
+        test_num=0,
+        test_name="ad-hoc"
     ):
-        if   mode == "debug":
-            path = os.path.join(os.path.dirname(__file__), "..", "x64", "Debug", "solution_data.csv")
-        elif mode == "release":
-            path = os.path.join(os.path.dirname(__file__), "..", "x64", "Release", "solution_data.csv")
-        else:
+        print("Initialising solution...")
+        
+        if mode != "debug" and mode != "release":
             EXIT_HELP()
         
-        print("Searching for solution data in", path)
+        self.test_num  = test_num
+        self.test_name = test_name
         
-        dataframe = pd.read_csv(path)
+        dataframe = pd.read_csv("solution_data.csv")
         
         self.x1  = dataframe["x1"].values
         self.x2  = dataframe["x2"].values
@@ -70,18 +90,51 @@ class Solution:
     def plot_soln(
         self
     ):
-        plot_soln(self.x1, self.x2, self.q,   "q",   self.length, "$q \, (m^2s^{-1})$")
-        plot_soln(self.x1, self.x2, self.eta, "eta", self.length, "$\eta \, (m)$")
-        plot_soln(self.x1, self.x2, self.z,   "z",   self.length, "$z \, (m)$")
-    
-def plot():
-    if len(sys.argv) > 2:
-        dummy, action, mode = sys.argv
+        print("Plotting solution...")
         
-        Solution(mode).plot_soln()
+        plot_soln(self.x1, self.x2, self.q,   "q",   self.length, "$q \, (m^2s^{-1})$", self.test_num, self.test_name)
+        plot_soln(self.x1, self.x2, self.eta, "eta", self.length, "$\eta \, (m)$", self.test_num, self.test_name)
+        plot_soln(self.x1, self.x2, self.z,   "z",   self.length, "$z \, (m)$", self.test_num, self.test_name)
+    
+def run():
+    print("Attempting to run...")
+    
+    if len(sys.argv) > 6:
+        dummy, action, mode, test, num_cells, max_ref_lvl, epsilon = sys.argv
+        
+        if   mode == "debug":
+            solver_file = os.path.join(os.path.dirname(__file__), "..", "x64", "Debug", "FV1_cpp.exe")
+        elif mode == "release":
+            solver_file = os.path.join(os.path.dirname(__file__), "..", "x64", "Release", "FV1_cpp.exe")
+        else:
+            EXIT_HELP()
+        
+        subprocess.run( [solver_file, str(test), num_cells, max_ref_lvl, epsilon] )
+        Solution(mode, test, test_names[int(test) - 1]).plot_soln()
     else:
         EXIT_HELP()
         
+def run_tests():
+    print("Attempting to run all tests...")
+    
+    if len(sys.argv) > 5:
+        dummy, action, mode, num_cells, max_ref_lvl, epsilon = sys.argv
+        
+        if   mode == "debug":
+            solver_file = os.path.join(os.path.dirname(__file__), "..", "x64", "Debug", "HFV1_cpp.exe")
+        elif mode == "release":
+            solver_file = os.path.join(os.path.dirname(__file__), "..", "x64", "Release", "HFV1_cpp.exe")
+        else:
+            EXIT_HELP()
+        
+        tests = [1, 2, 3, 4, 5, 6]
+        
+        for i, test in enumerate(tests):
+            subprocess.run( [solver_file, str(test), num_cells, max_ref_lvl, epsilon] )
+            Solution(mode, test, test_names[i]).plot_soln()
+    else:
+        EXIT_HELP()
+
 params = {
     "legend.fontsize" : "xx-large",
     "axes.labelsize"  : "xx-large",
@@ -95,12 +148,11 @@ pylab.rcParams.update(params)
 if len(sys.argv) > 1:
     action = sys.argv[1]
     
-    if action == "plot":
-        plot()
+    if action == "run":
+        run()
+    elif action == "test":
+        run_tests()
     else:
         EXIT_HELP()
 else:
     EXIT_HELP()
-   
-
-
